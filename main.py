751d4510434d7a789ccdc79de547d6a1db56ba5d
bullbesh <babulehaha@gmail.com>
2022-07-04 21:40:14 +0300

    Добавление в файл main.py функцию для информационного блока

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text as TextFilter
+
+import keyboard as kb
 
 
 logging.basicConfig(level=logging.INFO)
@@ -16,7 +19,15 @@ dp = Dispatcher(bot, storage=storage)
 @dp.message_handler(commands="start")
 async def greeting(message):
     """Greeting bot message."""
-    await message.answer("Первое сообщение")
+    await message.answer("Первое сообщение", reply_markup=kb.MainMenuMarkup)
+
+
+@dp.message_handler(TextFilter(equals=kb.InformationBlock))
+async def send_information_block(message):
+    """Relocate user to unformaion block."""
+    await message.answer(
+        "Выберите интересующую Вас тему",
+    )
 
 
 def main():
